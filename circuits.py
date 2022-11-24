557f6f5cc7fd7574801c6e89601293341fcbbc17
davide1729 <arnaldodabrescia90@gmail.com>
2022-11-24 19:44:59 +0100

    add use db

--- circuits.py
+++ circuits.py
@@ -4,23 +4,24 @@ import pandas as pd
 import mysql.connector
 import pandas as pd
 from mysql.connector import errorcode
+from Cleaning import *
+from connectionPythonMySql import *
 
-circuits = pd.read_csv('archive/circuits.csv')
-db_name = 'Formula1_database'
+print(circuits.info())
 
-mydb = mysql.connector.connect(
+'''mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="ciaociao",
   database= db_name
-)
+)'''
 
 mycursor = mydb.cursor()
 # Insert DataFrame records one by one.
 
 for i,row in circuits.iterrows():
             sql = "INSERT INTO db_name.CIRCUITS VALUES (%s,%s,%s,%s,%s)"
-            mycursor.execute(sql, tuple([row['circuitId'], row['circuitRef'], row['name_'], row['location'], row['country'], row['lat'], row['lng'], row['alt'], row['url']]))
+            mycursor.execute(sql, tuple([row['circuitId'], row['circuitRef'], row['name'], row['location'], row['country']]))
             #print("Record inserted")
             mydb.commit()
 
