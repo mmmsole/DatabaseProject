e0c8691085b6e08edda3d8fcdc99dab6df5b7310
demetriofcardile <cardile.demetrio02@gmail.com>
2022-11-24 18:07:48 +0100

    indici corretti

--- Cleaning.py
+++ Cleaning.py
@@ -9,7 +9,7 @@ results_1 = pd.read_csv('archive/results.csv')
 
 # FROM RACES GET YEAR < 2002
 races = races_1.loc[races_1["year"] > 2002]
-races = races.iloc[:,0:6]
+races = races.iloc[:,0:7]
 #to get some info about the table
 #print(races.info())
 #print(races_1.shape)
@@ -18,15 +18,15 @@ races = races.iloc[:,0:6]
 
 # GET LAP TIMES WITH RACE ID
 lap_times = lap_times_1[lap_times_1['raceId'].isin(races['raceId'])]
-lap_times = lap_times.iloc[:,[0,1,2,3,4,5]]
+lap_times = lap_times.iloc[:,0:6]
 #to get some info about the table
-#print(lap_times.info())
+print(lap_times.info())
 #print(lap_times_1.shape)
 #print(lap_times.shape)
 
 # GET PIT STOP WITH RACE ID
 pit_stops = pit_stops_1[pit_stops_1['raceId'].isin(races['raceId'])]
-pit_stops = pit_stops.iloc[:,[0,1,2,3,4,5]]
+pit_stops = pit_stops.iloc[:,0:6]
 #to get some info about the table
 #print(pit_stops.info())
 #print(pit_stops_1.shape)
@@ -49,7 +49,7 @@ drivers = drivers.iloc[:,:-1]
 #print(drivers.shape)
 
 circuits = circuits_1.iloc[:, 0:5]
-print(circuits.info())
+#print(circuits.info())
 
 
 
