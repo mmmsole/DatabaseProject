dd4ac51d84f16407ee4675eea4c92515ae5ba8c5
davide1729 <arnaldodabrescia90@gmail.com>
2022-12-03 17:13:24 +0100

    appv2 createDB adj

--- app_v2.py
+++ app_v2.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import Error, errorcode
 import pandas as pd
 
 db_name = 'F1_db'
@@ -18,12 +18,10 @@ else:
     user = input('What is your name?\n> ')
     pw = input('Enter your password, please.\n> ')
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=pw,
-    database='F1_db'
-)
+mydb = mysql.connector.connect(host='localhost',
+                                   user='root',
+                                   password=pw,
+                                   auth_plugin='mysql_native_password')
 
 
 def load_data():
@@ -38,7 +36,7 @@ def load_data():
                     mydb.commit()  # make the changes official
                     print("The database already exists! The old database has been deleted!")
 
-            mycursor.execute("CREATE DATABASE IF NOT EXISTS" + db_name)
+            mycursor.execute("CREATE DATABASE " + db_name)
             mycursor.execute("USE " + db_name)
     except errorcode as e:
         print("Error while connecting to MySQL", e)
